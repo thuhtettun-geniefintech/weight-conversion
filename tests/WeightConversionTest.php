36e43aeb14b9cfa5121db10d40e469df286bb405
thuhtettun-geniefintech <thuhtettun@geniefintech.com>
2022-07-13 16:51:08 +0630

    feat: add_stones

--- tests/WeightConversionTest.php
+++ tests/WeightConversionTest.php
@@ -6,3 +6,8 @@ it('can convert kilograms to pounds', function () {
     $pounds = WeightConversion::kilogram(100)->toPounds();
     expect($pounds)->toEqual(220.46);
 });
+
+it('can convert kilograms to stones', function () {
+    $pounds = WeightConversion::kilogram(100)->toStones();
+    expect($pounds)->toEqual(154.29999999999998);
+});
